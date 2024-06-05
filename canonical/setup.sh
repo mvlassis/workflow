bc76d1d77e63b0e32511ef46979e07fce8592161
mvlassis <accounts@mvlassis.com>
2024-06-05 15:00:02 +0300

    Auto commit at 2024-06-05T15:00:02 EEST

--- canonical/setup.sh
+++ canonical/setup.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
-sudo snap install snapcraft --classic
+sudo apt install gnome-keyring -y
+sudo snap install charmcraft --classic
+lxd init --auto
 sudo snap install docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
