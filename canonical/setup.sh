85ec8780bf9df2ebe2c5852e39d81a7794b2d2c6
mvlassis <accounts@mvlassis.com>
2024-06-07 12:00:57 +0300

    Auto commit at 2024-06-07T12:00:57 EEST

--- canonical/setup.sh
+++ canonical/setup.sh
@@ -3,9 +3,19 @@
 sudo apt install libffi-dev -y
 sudo apt install tox -y
 sudo apt install gnome-keyring -y
+sudo snap install microk8s --classic
 sudo snap install charmcraft --classic
+sudo snap install rockcraft --classic
+
 lxd init --auto
+adduser ubuntu lxd
+
 sudo snap install docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+
+# Setup Juju
+sudo snap install juju
+juju bootstrap localhost lxd
+juju bootstrap microk8s microk8s
