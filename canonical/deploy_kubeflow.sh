f5719fa3bf68c40dae7258dd68f5e0f134d6ecd8
mvlassis <accounts@mvlassis.com>
2024-06-20 14:00:06 +0300

    Auto commit at 2024-06-20T14:00:06 EEST

--- canonical/deploy_kubeflow.sh
+++ canonical/deploy_kubeflow.sh
@@ -7,31 +7,31 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # Install and setup Microk8s
-sudo apt update
-sudo snap install microk8s --classic --channel=1.26/stable
-sudo usermod -a -G microk8s $USER
+apt update
+snap install microk8s --classic --channel=1.26/stable
+usermod -a -G microk8s $USER
 newgrp microk8s
-sudo chown -f -R $USER ~/.kube
+chown -f -R $USER ~/.kube
 
 # Enable some Microk8s addons
 microk8s enable dns hostpath-storage ingress metallb:10.64.140.43-10.64.140.49 rbac
 
 # Install and setup Juju
-sudo snap install juju --classic --channel=3.1/stable
+snap install juju --classic --channel=3.1/stable
 mkdir -p ~/.local/share
 microk8s config | juju add-k8s my-k8s --client
 juju bootstrap my-k8s uk8sx
 juju add-model kubeflow
 
 # Deploy Charmed Kubeflow
-sudo sysctl fs.inotify.max_user_instances=1280
-sudo sysctl fs.inotify.max_user_watches=655360
+sysctl fs.inotify.max_user_instances=1280
+sysctl fs.inotify.max_user_watches=655360
 if ! grep -q "^fs.inotify.max_user_instances" /etc/sysctl.conf; then
-    echo "fs.inotify.max_user_instances=1280" | sudo tee -a /etc/sysctl.conf
+    echo "fs.inotify.max_user_instances=1280" | tee -a /etc/sysctl.conf
 fi
 
 if ! grep -q "^fs.inotify.max_user_watches" /etc/sysctl.conf; then
-    echo "fs.inotify.max_user_watches=655360" | sudo tee -a /etc/sysctl.conf
+    echo "fs.inotify.max_user_watches=655360" | tee -a /etc/sysctl.conf
 fi
 
 juju deploy kubeflow --trust  --channel=1.8/stable
