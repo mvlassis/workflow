19b22fcd050c16f37db0b0aa40f0c2b824affe4b
mvlassis <manos.vlassis@canonical.com>
2025-02-06 16:00:12 +0200

    Auto commit at 2025-02-06T16:00:12 EET

--- canonical/setup.sh
+++ canonical/setup.sh
@@ -2,6 +2,7 @@
 
 setup_canonical_dev() {
 	sudo apt install python3-pip -y
+	sudo apt install python3-venv -y
 	sudo snap install charmcraft --classic
 	sudo snap install rockcraft --classic
 	sudo snap install docker
