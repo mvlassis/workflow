56ea9b99ca113c91bf476f4480a41e83644aa88c
mvlassis <manos.vlassis@canonical.com>
2025-09-03 13:09:54 +0300

    Update concierge.yaml

--- canonical/setup.sh
+++ canonical/setup.sh
@@ -5,5 +5,5 @@ sudo snap install concierge --classic
 sudo apt-get remove -y docker-ce docker-ce-cli containerd.io
 sudo rm -rf /run/containerd
 
-sudo concierge prepare "$(dirname "${BASH_SOURCE[0]}")/concierge.yaml"
+sudo concierge prepare -c "$(dirname "${BASH_SOURCE[0]}")/concierge.yaml"
 
